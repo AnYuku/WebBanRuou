ed459545772a04a2991726378799383f297165b3
Vunguyen20122002 <baycaohon1@gmail.com>
2023-04-26 15:07:14 +0700

    26/04/2023

--- WEBBANRUOU/js/jsPage.js
+++ WEBBANRUOU/js/jsPage.js
@@ -1,34 +1,4 @@
 
-// Hover list menu user
-var mennuHoverUser = document.querySelector('.link_user_hover');
-var menuUser = document.querySelector('.listMenuUser');
-
-mennuHoverUser.addEventListener('mouseenter', function() {
-    menuUser.style.display = 'block';
-    isMenuVisible = true;
-  });
-  
-  mennuHoverUser.addEventListener('mouseleave', function() {
-    isMenuVisible = false;
-    setTimeout(function() {
-      if (!isMenuVisible) {
-        menuUser.style.display = 'none';
-      }
-    }, 200);
-  });
-  
-  menuUser.addEventListener('mouseenter', function() {
-    isMenuVisible = true;
-  });
-  
-  menuUser.addEventListener('mouseleave', function() {
-    isMenuVisible = false;
-    setTimeout(function() {
-      if (!isMenuVisible) {
-        menuUser.style.display = 'none';
-      }
-    }, 200);
-  });
 
 
   
